6af84121d4ced606415966a14e492ce261fda639
harrihonkanen <devharri@outlook.com>
2023-02-03 16:58:49 +0200

    initial commit

---
+++ UploadFilesLibrary/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace UploadFilesLibrary.DataAccess;
+
+public interface ISqlDataAccess
+{
+    Task<List<T>> LoadData<T>(string storedProc, string connectionName, object? parameters);
+    Task SaveData(string storedProc, string connectionName, object parameters);
+}
---
+++ UploadFilesLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using MySql.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace UploadFilesLibrary.DataAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<List<T>> LoadData<T>(string storedProc, string connectionName, object? parameters)
+    {
+        string connectionString = _config.GetConnectionString("Default") ?? throw new Exception($"Connection string missing at {connectionName}");
+
+        using var connection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+
+        var rows = await connection.QueryAsync<T>(storedProc, parameters, commandType: System.Data.CommandType.StoredProcedure);
+
+        return rows.ToList();
+    }
+
+    public async Task SaveData(string storedProc, string connectionName, object parameters)
+    {
+        string connectionString = _config.GetConnectionString("Default") ?? throw new Exception($"Connection string missing at {connectionName}");
+
+        using var connection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+
+        await connection.ExecuteAsync(storedProc, parameters, commandType: System.Data.CommandType.StoredProcedure);
+        
+    }
+}
